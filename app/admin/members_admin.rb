10d1458628a088222993028f14ab66c0ba031926
Benjamin <benjamin.guy.thomas@gmail.com>
2021-02-07 05:44:34 +0100

    Improve views

--- app/admin/members_admin.rb
+++ app/admin/members_admin.rb
@@ -11,14 +11,28 @@ Trestle.resource(:members) do
     column :family
   end
 
+  # attrs - permitted params for the resource
+  # params - full params hash
+  build_instance do |attrs, params|
+    Member.new(attrs).tap do |member|
+      # http://rails-erp.auja.test:3000/admin/members/new?family_id=4
+      if (family_id=params[:family_id])
+        member.family_id = family_id
+        member.last_name = Family.find(family_id).name
+      end
+    end
+  end
+
   form do |member|
     if !member.persisted? || params[:verbose] == '1'
-      select :family_id, Family.order(:name)
+      select :family_id, Family.order(:name), label: t('activerecord.models.family.one').capitalize
     end
 
     row do
       col { text_field :first_name }
       col { text_field :last_name }
     end
+
+    email_field :email
   end
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,7 +1,13 @@
 class Family < ApplicationRecord
   has_many :members
 
+  validates :name, presence: true
+
   def self.search(query)
     where('families.name ILIKE ?', "%#{query}%")
+  end
+
+  def to_s
+    name
   end
 end
